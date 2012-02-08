5ab5468d21829bf8a12517265eb0eb2e9180d3c1
Jollyrancher <Jollyrancher@Jollyrancher-PC>
2012-02-08 15:52:10 -0500

    Backed out changeset: 8a720e599f78

--- data/scripts/locations.sql
+++
@@ -1,9 +0,0 @@
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
-    CREATE TABLE locations (
-        location_id text collate nocase NOT NULL PRIMARY KEY,
-        max_storage integer NOT NULL
-    );
-INSERT INTO "locations" VALUES('Binder A', 500);
-INSERT INTO "locations" VALUES('Binder B', 500);
-COMMIT;
